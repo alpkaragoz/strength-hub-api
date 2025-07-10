a31f092ce4c1e9acb52f79c5ea65da257ff84fc9
Alp Karagöz <alpkaragoz17@ku.edu.tr>
2025-07-10 17:16:52 +0300

    first commit

---
+++ src/main/java/com/strengthhub/strength_hub_api/StrengthHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.strengthhub.strength_hub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrengthHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrengthHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strengthhub/strength_hub_api/StrengthHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strengthhub.strength_hub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrengthHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
